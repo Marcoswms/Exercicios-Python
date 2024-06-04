63c93153052c71fb2b9181ff2204546ef89123bd
Marcoswms <86364619+Marcoswms@users.noreply.github.com>
2024-06-04 16:32:57 -0300

    Update ex_05_listas.py
    Alterado o valor da constante "LIMITE_DE_VALORES_NUMERICOS" de 04 foi para 10, assim como é solicitado no enunciado

--- Aula_10_Listas/ex_05_listas.py
+++ Aula_10_Listas/ex_05_listas.py
@@ -5,7 +5,7 @@ os.system("cls")
 print("\nPrograma que receba uma lista com 10 valores numéricos, gere uma nova lista onde cada"
       "elemento dessa lista é o quadrado dos elementos da primeira lista.\n")
 
-LIMITE_DE_VALORES_NUMERICOS = 4
+LIMITE_DE_VALORES_NUMERICOS = 10
 
 listaValoresNumericos = []
 listaQuadradoDosValores = []
